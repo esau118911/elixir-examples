4ef1918436c444e3252e83cc4935590c4fb2bf80
Esau Hernandez <narvaez_1189@hotmail.com>
2024-10-17 19:54:50 -0600

    Se ajusta doc

--- inmutabilidad/inmutable.exs
+++ inmutabilidad/inmutable.exs
@@ -2,7 +2,7 @@ defmodule Inmutable do
   # Ejemplos de inmutabilidad en funciones
   #  x = 5
   #  y = Inmutable.reducir(x)
-  #  creamos un nuevo numero y x no altera o cambia su valor
+  #  creamos un nuevo numero 4 y x no se altera o cambia su valor
 
   def incrementar(x), do: x + 1
   def reducir(x), do: x - 1
--- operadores/comparacion2.exs
+++ operadores/comparacion2.exs
@@ -1,6 +1,6 @@
 edad = 1
 mensaje = cond do
-  edad === 1.0 -> "Es un bebé condicion estricta"
+  edad === 1.0 -> "Es un bebé menor de un año"
   edad <= 5 -> "Es un bebe"
   edad >= 18 -> "Es un adulto"
 end
