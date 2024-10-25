0bbf691f7ab0b4cbbeb73a21baab62fffc25dae4
Esau Hernandez <narvaez_1189@hotmail.com>
2024-10-25 12:58:53 -0600

    Actualizas cambios

--- colecciones/maps.exs
+++ colecciones/maps.exs
@@ -8,8 +8,11 @@ person = %{
 IO.inspect("Acceso a valor nombre: #{person[:name]}")
 IO.inspect("Acceso a valor edad: #{person[:age]}")
 
-# Actualizar la edad
-updated_age = Map.put(person, :age, 35)
+# Actualizar la edad  cuando la clave existe
+updated_age = %{person | age: 35}
+
+# Actualizar la edad insertando una clave nueva
+insert_age = Map.put(person, :age, 38)
 
 # Eliminar la clave :edad
 delete_age = Map.delete(person, :age)
@@ -24,6 +27,7 @@ Enum.each(person, fn {key, value} -> IO.inspect("#{key}: #{value}") end)
 names = Enum.map(person, fn {key, value} -> value end)
 
 IO.inspect(updated_age)
+IO.inspect(insert_age)
 IO.inspect(delete_age)
 IO.inspect(exists_city)
 IO.inspect(names)
